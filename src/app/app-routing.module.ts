b2e0929d2298311289ec5e4943dc2b9199ba6533
Lawrence Elango <elangolawrence@gmail.com>
2023-06-19 13:28:44 +0100

    +

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from './theme/layout/layout.component';
-import { PageNotFoundComponent } from './components/sections/page-not-found/page-not-found.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LayoutComponent } from "./theme/layout/layout.component";
+import { PageNotFoundComponent } from "./components/sections/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/portfolio/me',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "/portfolio/me",
+    pathMatch: "full",
   },
   {
-    path: 'portfolio',
+    path: "portfolio",
     component: LayoutComponent,
     children: [
       {
-        path: 'me',
-        loadChildren: () => import('./view/view.module').then((module) => module.ViewModule)
-      }
-    ]
+        path: "me",
+        loadChildren: () =>
+          import("./view/view.module").then((module) => module.ViewModule),
+      },
+    ],
   },
   { path: "404", component: PageNotFoundComponent },
   { path: "**", redirectTo: "404" },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: "enabled",
+    }),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
